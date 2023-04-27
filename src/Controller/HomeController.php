4fec34f5ea6d50e96b78faa955fafdabb8c891bd
naomie marco <naomiemarco7@gmail.com>
2023-04-27 10:12:57 +0200

    feat : liste des question dans la homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,28 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $questions = [
+            [
+                'title'=> 'je suis une question',
+                'content'=> 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, quis odit! Odit earum quisquam ea animi in qui sit quia. Consequatur illum voluptas quidem, sed et                        numquam neque aspernatur quibusdam.',
+                'rating'=> 0,
+                'author' => [
+                    'name'=> 'Laure Joe',
+                    'avatar' => 'https://randomuser.me/api/portraits/women/79.jpg'
+                ],
+                'nbResponse'=> 5
+            ],
+            [
+                'title'=> 'je suis une question',
+                'content'=> 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, quis odit! Odit earum quisquam ea animi in qui sit quia. Consequatur illum voluptas quidem, sed et                        numquam neque aspernatur quibusdam.',
+                'rating'=> -15,
+                'author' => [
+                    'name'=> 'Pascal Praud',
+                    'avatar' => 'https://randomuser.me/api/portraits/men/40.jpg'
+                ],
+                'nbResponse'=> 25
+            ]
+        ];
+        return $this->render('home/index.html.twig', ['questions' => $questions]);
     }
 }
